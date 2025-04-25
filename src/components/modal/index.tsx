3f507f113496eaa741dae248765aaaba8b3efb4f
Antonio-Savio <antonio.savio28@gmail.com>
2025-04-25 00:19:15 -0300

    feat: adding loading functionality to gif images

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Projects } from "../../helpers/projects"
 import styles from './modal.module.css'
 import { IoCloseSharp } from "react-icons/io5";
+import { FaSpinner } from "react-icons/fa";
 
 interface ModalProps{
     prop: Projects;
@@ -10,6 +12,11 @@ interface ModalProps{
 }
 
 export function Modal({ prop, lang, onClose, darkMode }: ModalProps){
+    const [loading, setLoading] = useState(true)
+
+    function handleLoadGif(){
+        setLoading(false)
+    }
 
     return (
         <main className={styles.background}>
@@ -23,8 +30,23 @@ export function Modal({ prop, lang, onClose, darkMode }: ModalProps){
                         <source src={prop.video} type="video/webm" />
                     </video>
                 ): (
-                    <img src={prop.imgSrc} alt={prop.title[lang]} />
+                    <div className={styles.imgContainer}>
+                        <img 
+                            src={prop.imgSrc} 
+                            alt={prop.title[lang]} 
+                            onLoad={handleLoadGif}
+                            style={{ visibility: loading ? 'hidden' : 'visible' }}
+                        />
+
+                        {loading && prop.imgSrc && (
+                            <div className={loading ? styles.load : ''}>
+                                {lang === 'pt' ? 'Carregando GIF...' : 'Loading GIF...'}
+                                <FaSpinner className={styles.loadIcon} />
+                            </div>
+                        )}
+                    </div>
                 )}
+
                 <section className={styles.desc}>
                     {prop.description[lang]}
                 </section>
